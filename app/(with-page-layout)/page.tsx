e9fd0b713152fcf3a6a73522d90398e4b886964a
Marvin Scheffold <m@marvinscheffold.com>
2024-11-05 19:59:28 +0100

    refactor

--- app/(with-page-layout)/page.tsx
+++ app/(with-page-layout)/page.tsx
@@ -1,8 +1,3 @@
 export default function Page() {
-  const isDevelop = process.env.NODE_ENV! === "development";
-  return (
-    <div>
-      is develop {isDevelop} NODE_ENV: {process.env.NODE_ENV!}
-    </div>
-  );
+  return <div>Home</div>;
 }
