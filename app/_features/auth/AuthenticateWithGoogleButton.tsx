7fd29770de191e9947d02cb80f5f51e369f70e0c
Marvin Scheffold <m@marvinscheffold.com>
2024-11-05 17:52:08 +0100

    refactor google button

--- app/_features/auth/AuthenticateWithGoogleButton.tsx
+++ app/_features/auth/AuthenticateWithGoogleButton.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import { SubmitButton } from "@/app/_components/SubmitButton";
 import { authenticateWithGoogleAction } from "@/app/_features/auth/actions";
 import { Google } from "@mui/icons-material";
-import { LoadingButton } from "@mui/lab";
-import { useFormStatus } from "react-dom";
 
 type AuthenticateWithGoogleButtonProps = {
   redirectTo?: string;
@@ -14,23 +13,14 @@ export function AuthenticateWithGoogleButton({
 }: AuthenticateWithGoogleButtonProps) {
   return (
     <form action={() => authenticateWithGoogleAction(redirectTo)}>
-      <SubmitButton />
+      <SubmitButton
+        className="w-full"
+        startIcon={<Google />}
+        loadingPosition="end"
+        variant="outlined"
+      >
+        Mit Google fortfahren
+      </SubmitButton>
     </form>
-  );
-}
-
-function SubmitButton() {
-  const { pending } = useFormStatus();
-  return (
-    <LoadingButton
-      className="w-full"
-      startIcon={<Google />}
-      loadingPosition="end"
-      variant="outlined"
-      loading={pending}
-      type="submit"
-    >
-      Mit Google fortfahren
-    </LoadingButton>
   );
 }
